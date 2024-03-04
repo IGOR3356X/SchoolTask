6e4e0cd06b4122ffda7d8d36b06ddaaa4b1eac92
IGOR3356X <igor3356c@gmail.com>
2024-03-04 11:55:21 +0300

    GG

---
+++ File.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolTask
+{
+    internal class File
+    {
+        public string GetFile()
+        {
+            string Put = "C:\\Users\\igor3\\source\\repos\\SchoolTask\\bin\\Debug\\JsonText.json";
+            return Put;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SchoolTask.Entities;
+using SchoolTask.Realization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,21 +12,17 @@ namespace SchoolTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Добро пожалоать в приложение 'Школа'\nВыерите действие");
-            Console.ReadKey();
-            Console.WriteLine("1. Добавить ученика\n2. Вывести всех учителей");
+            InputStud inputStud = new InputStud();
+            Console.WriteLine("Добро пожалоать в приложение 'Школа'\nВыерите действие\n1. Добавить ученика\n2. Вывести всех учителей");
             var user_input = Convert.ToInt32(Console.ReadLine());
+
+
             if (user_input == 1)
             {
-                Console.WriteLine("Введите ид/Имя/Класс");
-                Console.Write("ID");
-                var ID = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Имя");
-                var Name = Convert.ToString(Console.ReadLine());
-                Console.Write("Класс");
-                var ClassStud = Convert.ToString(Console.ReadLine());
-
+                inputStud.Inputinfo();
             }
+
+            Console.ReadKey();
         }
     }
 }
